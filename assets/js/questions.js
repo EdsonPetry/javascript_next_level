3f57c83e97b225cb441f71896c7bfdf943a78600
Edson Petry <edsonlbpetry@gmail.com>
2023-03-17 10:03:03 -0400

    updating

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+      question:
+        "What language should you use to build the structure for your website?",
+      answers: [
+        { option: "HTML", correct: true },
+        { option: "JavaScript", correct: false },
+        { option: "CSS", correct: false },
+        { option: "Python", correct: false },
+      ],
+    },
+  
+    {
+      question: "How do you select an ID in your HTML from your stylesheet?",
+      answers: [
+        { option: "#", correct: true },
+        { option: ".", correct: false },
+        { option: ".getElementByID", correct: false },
+        { option: ";", correct: false },
+      ],
+    },
+  
+    {
+      question: "Which of the follow is a valid HTML tag?",
+      answers: [
+        { option: "h1", correct: false },
+        { option: "<>", correct: false },
+        { option: "<h1>", correct: true },
+        { option: ">h1,", correct: false },
+      ],
+    },
+  
+    {
+      question:
+        "Which of the following is an example of metadata about a webpage?",
+      answers: [
+        { option: "The title of the webpage", correct: true },
+        { option: "The body of the webpage", correct: false },
+        { option: "An <h1> tag on the webpage", correct: false },
+        { option: "All of the above", correct: false },
+      ],
+    },
+  
+    {
+      question: "What does the <br> tag do?",
+      answers: [
+        { option: "Creates a line break on the webpage", correct: true },
+        { option: "Creates a horizontal line on the webpage", correct: false },
+        { option: "Bolds text", correct: false },
+        { option: "None of the above", correct: false },
+      ],
+    },
+  ];
